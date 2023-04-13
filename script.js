cd27536fbb06e846d450a25a5cce206e4680ca1a
Abdisalam <abdisalamfresh@gmail.com>
2023-04-12 23:33:07 -0700

    Introduced myself with an inline javaScript

---
+++ script.js
@@ -0,0 +1,4 @@
+const firstName = 'Abdisalam';
+const lastName = 'Kadir';
+const yearsOfStudy = 1;
+const goal = 'My goal is to study software development and one day get a job in software development,';
